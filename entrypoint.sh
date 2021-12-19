c05bb8df61e353fd6c7fbfab52450d3d50e7e35b
Riccardo Bello <riccardobello98@gmail.com>
2021-12-19 22:54:48 +0100

    Hide startup command

--- entrypoint.sh
+++ entrypoint.sh
@@ -22,7 +22,7 @@ fi
 
 # Replace Startup Variables
 MODIFIED_STARTUP=`eval echo $(echo ${STARTUP} | sed -e 's/{{/${/g' -e 's/}}/}/g')`
-echo ":/home/container$ ${MODIFIED_STARTUP}"
+# echo ":/home/container$ ${MODIFIED_STARTUP}" # don't show, might contain sensitive information
 
 # Run the Server
 eval ${MODIFIED_STARTUP}
